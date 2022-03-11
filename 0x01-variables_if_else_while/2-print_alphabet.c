0fd06164bcc16fa42d58ea8e56e0cebd3ec2ff30
Tofik <gashawtofik@gmail.com>
2022-03-12 00:11:26 +0300

    Task 2 commit for 0x01 C project

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,13 @@
  * followed by a new line
  * Return: Always 0 (Success)
       */
+
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);					
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
