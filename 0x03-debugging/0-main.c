b210728a44f9cb6187a505f0066b3916f644fc55
Tofik <gashawtofik@gmail.com>
2022-03-17 02:21:39 +0300

    task 0 debeg commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /*
  * main - test function that prints if integer is positive or negative
- * return: always 0 for (success)
+ * return:  0 
  */
 
 int main(void)
@@ -12,6 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 	
-
-	return (0);
+return (0);
 }
