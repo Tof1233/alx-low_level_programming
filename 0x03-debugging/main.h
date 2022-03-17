9e1789f26724a77d8a59886fe19cc73d490c756d
Tofik <gashawtofik@gmail.com>
2022-03-17 03:22:54 +0300

    task 0 debeg commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,8 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
-/*Prints whether the number stored in a variable is positive or negative*/
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
