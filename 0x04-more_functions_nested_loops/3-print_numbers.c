2f44c2ede0e1f8728abd770ce6e559fb88dd00df
Tofik <gashawtofik@gmail.com>
2022-03-18 03:32:41 +0300

    3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * 0123456789
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
