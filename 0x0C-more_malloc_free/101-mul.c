61956758b825be417d506b634d0cb82d5886acde
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-10 01:06:09 +0300

    commit on task 101-mul tryal

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,13 +1,11 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-int is_digit(char *s)
-int _strlen(char *s)
-void errors(void)
+#include "main.h"
 
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
+ *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
@@ -26,6 +24,7 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
+ *
  * Return: the length of the string
  */
 int _strlen(char *s)
@@ -52,6 +51,7 @@ void errors(void)
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
