2dfbc5b4df9826a7819c0a34ca411f889ff28b07
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-10 03:08:48 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,5 +13,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_digit(char *s);
 char *_initialize_array(char *ar, int lar);
 int _checknum(char *argv[], int n);
-void errors(void)
+void errors(void);
+ 
 #endif /* MAIN_H */
